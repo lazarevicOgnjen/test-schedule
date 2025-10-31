c33610edefecc7f46e1a322f277ee9bc20ee12f8
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-31 22:18:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ for row in rows:
     cols = row.xpath('.//td')
     if len(cols) >= 8:
         row_values = [col.text_content().strip() for col in cols]
-        if ("3" in row_values or "4" in row_values) and "РИИ" in row_values:
+        if ("3" in row_values or "4" in row_values) and "РИИ" in row_values and "2019" in row_values:
             filtered_rows.append(row_values)
 
 with open("README.md", "w", encoding="utf-8") as f:
